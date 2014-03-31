9bad34c4d983555b82730b10270ab86dc1f3c169
Martin Page <martin.page@tsl.ac.uk>
2014-03-31 11:33:01 +0100

    js fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,5 +3,18 @@
 //= require bootstrap
 //= require jquery.ui.autocomplete
 //= require jquery.bgiframe.min
-//= require bootstrap.filestyle
 //= require_tree .
+
+jQuery(function() {
+  $("a[rel=popover]").popover();
+  $(".tooltip").tooltip();
+  return $("a[rel=tooltip]").tooltip();
+});
+
+$(document).ready(function() {
+  return $(":file").filestyle({
+    icon: true,
+    classIcon: "icon-file",
+    buttonText: "Choose file..."
+  });
+});
