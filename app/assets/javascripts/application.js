f75e73bb1edb2074ba74b4bf8e5354c1bc48021b
Martin Page <martin.page@tsl.ac.uk>
2014-04-01 17:15:30 +0100

    buggy coffee

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,16 +5,16 @@
 //= require jquery.bgiframe.min
 //= require_tree .
 
-jQuery(function() {
-  $("a[rel=popover]").popover();
-  $(".tooltip").tooltip();
-  return $("a[rel=tooltip]").tooltip();
-});
+// jQuery(function() {
+//   $("a[rel=popover]").popover();
+//   $(".tooltip").tooltip();
+//   return $("a[rel=tooltip]").tooltip();
+// });
 
-$(document).ready(function() {
-  return $(":file").filestyle({
-    icon: true,
-    classIcon: "icon-file",
-    buttonText: "Choose file..."
-  });
-});
+// $(document).ready(function() {
+//   return $(":file").filestyle({
+//     icon: true,
+//     classIcon: "icon-file",
+//     buttonText: "Choose file..."
+//   });
+// });
